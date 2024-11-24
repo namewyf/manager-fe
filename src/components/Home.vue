357b3d0990e68cfec06285f8614c73d22a0dcad1
namewyf <wyf19892552132@163.com>
2024-11-24 18:27:28 +0800

    首页侧边菜单展开折叠

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="basic-layout">
     <!-- 导航栏 -->
-    <div class="nav-side">
+    <div :class="['nav-side', isCollapse ? 'fold' : 'unfold']">
       <!-- 系统logo -->
       <div class="logo">
         <img src="./../assets/logo.png" alt="">
         <span>Manager</span>
       </div>
       <!-- 导航菜单 -->
-      <el-menu default-active="2" background-color="#001529" text-color="#fff" router :collapse="false"
+      <el-menu default-active="2" background-color="#001529" text-color="#fff" router :collapse="isCollapse"
         class="nav-menu">
         <el-sub-menu index="1">
           <!-- 这个地方的template是插槽语法，#title是v-slot的缩写，表示将内容插入到父组件中名为 title 的插槽中。 -->
@@ -36,11 +36,11 @@
       </el-menu>
     </div>
     <!-- 右侧内容 -->
-    <div class="content-right">
+    <div :class="['content-right', isCollapse ? 'fold' : 'unfold']">
       <!-- 顶部导航栏 -->
       <div class="nav-top">
         <div class="nav-left">
-          <div class="menu-fold">
+          <div class="menu-fold" @click="toggle">
             <el-icon>
               <Operation />
             </el-icon>
@@ -51,7 +51,25 @@
         </div>
 
         <div class="user-info">
-          用户名
+          <el-badge :is-dot="true" class="notice">
+            <el-icon>
+              <Bell />
+            </el-icon>
+          </el-badge>
+          <el-dropdown @command="handleLogout">
+            <span class="user-link">
+              {{ userInfo.userName }}
+              <el-icon class="el-icon--right">
+                <arrow-down />
+              </el-icon>
+            </span>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item command="email">邮箱：{{ userInfo.userEmail }}</el-dropdown-item>
+                <el-dropdown-item command="logout">退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
         </div>
       </div>
       <div class="wrapper">
@@ -69,9 +87,26 @@ export default {
   components: {
   },
   data() {
-    return {}
+    return {
+      userInfo: {
+        userName: 'admin',
+        userEmail: 'admin@qq.com'
+      },
+      isCollapse: false
+    }
   },
-  methods: {},
+  methods: {
+    toggle() {
+      this.isCollapse = !this.isCollapse
+    },
+    handleLogout(key) {
+      if (key == 'logout') {
+        this.$store.commit('saveUserInfo', '')
+        this.userInfo = null
+        this.$router.push('/login')
+      }
+    }
+  },
   mounted() { }
 }
 </script>
@@ -87,6 +122,7 @@ export default {
     background-color: #001529;
     color: #fff; //文字颜色为白色
     overflow-y: auto; //滚动条
+    overflow-x: hidden;
     transition: width 0.5s; //宽度变化的动画
 
     .logo {
@@ -106,10 +142,29 @@ export default {
       border-right: none;
       height: calc(100vh - 50px);
     }
+
+    //在 SCSS 中，& 符号代表当前父选择器。当你在一个嵌套的选择器中使用 & 时，它会将父选择器与子选择器组合在一起。
+    //合并
+    &.fold {
+      width: 64px;
+    }
+
+    //展开
+    &.unfold {
+      width: 200px;
+    }
   }
 
   .content-right {
     margin-left: 200px;
+
+    &.fold {
+      margin-left: 64px;
+    }
+
+    &.unfold {
+      margin-left: 200px;
+    }
 
     .nav-top {
       height: 50px;
@@ -118,6 +173,33 @@ export default {
       justify-content: space-between; //控制两段对齐
       border-bottom: 1px solid #ddd;
       padding: 0 20px;
+
+      .nav-left {
+        display: flex;
+        align-items: center;
+
+        .menu-fold {
+          margin-right: 15px;
+          font-size: 18px;
+        }
+      }
+
+      .user-info {
+        display: flex;
+        align-items: center;
+
+        .notice {
+          line-height: 30px;
+          margin-right: 15px;
+        }
+
+        .user-link {
+          cursor: pointer;
+          color: #409eff;
+          display: flex;
+          align-items: center;
+        }
+      }
     }
 
     .wrapper {
