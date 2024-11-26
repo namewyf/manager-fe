add0d9202418abc87d5d9d9e0a2d50d6ca0b27ab
namewyf <wyf19892552132@163.com>
2024-11-26 11:36:38 +0800

    配置路径别名，将./../src配置为@

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../components/Home.vue'
+import Home from '@/components/Home.vue'
 
 
 const routes = [
@@ -18,7 +18,7 @@ const routes = [
                 meta: {
                     title: '欢迎页'
                 },
-                component: ()=> import('../views/Welcome.vue')
+                component: () => import('@/views/Welcome.vue')
             }
         ]
     },
@@ -30,7 +30,7 @@ const routes = [
         },
         //这里实现了路由的按需加载，动态导入，如果把import放在顶部引入，那么首页加载的时候就会加载所有组件，导致首页加载速度变慢
         //这里的import是一个函数，返回一个promise，在promise中返回一个组件，在加载的时候才加载组件
-        component: () => import('../views/Login.vue')
+        component: () => import('@/views/Login.vue')
     }
 ]
 
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/manager-fe/', // github仓库名称
   plugins: [vue()],
-
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   //配置 Vite 项目中 SCSS 预处理器的 API 类型，指定使用现代编译器。这有助于确保 SCSS 文件在编译时使用最新的编译方式，提高兼容性和性能。
   css: {
     preprocessorOptions: {
       scss: {
+        // additionalData: `@import "@/assets/style/base.scss";`,
         api: "modern-compiler" // or 'modern'
       }
     }
